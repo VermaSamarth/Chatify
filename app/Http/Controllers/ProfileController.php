3486a2d7585d09992bfcd40c69290a87b1759e9f
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2023-09-05 11:32:49 +0530

    First commit: Half Database created and Registration updated.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -26,8 +26,8 @@ class ProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+        Auth::user()->phone = $request->phone;
         $request->user()->fill($request->validated());
-
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
